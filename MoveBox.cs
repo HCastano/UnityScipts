f0bc8c92312d842c54b2bb9817d1b242b1f8d5be
Kcaj Chen <Jack@v1020-wn-108-218.campus-dynamic.uwaterloo.ca>
2015-11-14 23:23:37 -0500

    update script

--- MoveBox.cs
+++ MoveBox.cs
@@ -1,10 +1,15 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Resources;
+using LockingPolicy = Thalmic.Myo.LockingPolicy;
+using Pose = Thalmic.Myo.Pose;
+using UnlockType = Thalmic.Myo.UnlockType;
+using VibrationType = Thalmic.Myo.VibrationType;
+
 
 public class MoveBox : MonoBehaviour
 {
-
+	public GameObject myo = null;
     float moveSpeed = 0f;
     float moveAccel = 6.0f;
 
@@ -14,40 +19,27 @@ public class MoveBox : MonoBehaviour
 
 
     }
+	//private Pose _lastPose = Pose.Unknown;
 
 
     // Update is called once per frame
     void Update()
     {
+		ThalmicMyo thalmicMyo = myo.GetComponent<ThalmicMyo> ();
+
         //a = v/t
         //v = a * t 
 
         transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
 
-        while (Abs(moveSpeed) < 2)
-        {
-            if (Input.GetKey(KeyCode.UpArrow))
-            {
-                moveSpeed += moveAccel * Time.deltaTime;
-            }
+            if (thalmicMyo.pose==Pose.WaveIn) moveSpeed += moveAccel * Time.deltaTime;
+            
 
-            if (Input.GetKey(KeyCode.DownArrow))
-            {
-                moveSpeed -= moveAccel * Time.deltaTime;
+            if (thalmicMyo.pose==Pose.WaveOut) moveSpeed -= moveAccel * Time.deltaTime;
 
-            }
-        }
+            
+        
     }
 
-    void FixedUpdate()
-    {
-
-    }
-
-    float Abs(float num)
-    {
-        if (num < 0) return -1 * num;
-
-        return num;
-    }
+  
 }
